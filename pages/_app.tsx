7a9617afd3a71b7a00c08bc387f5ca1bf25cdf2e
Cadu de Castro Alves <cadudecastroalves@gmail.com>
2022-11-23 21:25:58 +0100

    First release

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { BaukastenProvider } from "@hygraph/baukasten";
+
+import "@fontsource/inter/variable-full.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <BaukastenProvider>
+            <Component {...pageProps} />
+        </BaukastenProvider>
+    );
 }
